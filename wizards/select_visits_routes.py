5a77eb9495e62badb4fc59ca21bea4e2cbd0c4eb
danigarciamendez <b3garciadaniel@gmail.com>
2019-10-25 13:49:56 +0200

    falta report

--- wizards/select_visits_routes.py
+++ wizards/select_visits_routes.py
@@ -11,23 +11,23 @@ class PartnerVisitDay(models.TransientModel):
     _name = 'partner.visit.day'
     _description = 'Partner Visit Day'
 
-    user_id = fields.Many2one('res.users')
-    partner_name = fields.Char(string='Partner Name')
-    visit_next_day = fields.Date(string='Next Visit')
+    user_id = fields.Many2one('res.users', 'Selection User')
+    date_visit = fields.Date(string='Next Visit')
 
-    @api.onchange('user_id')
-    def _onchange_user_id(self):
-        logging.info("----------------------------------")
-        logging.info(self.user_id)
-        for partner in self.user_id.partner_ids:
-            # logging.info(partner.name)
-            for visit in partner.visit_ids:
-                logging.info("----------------------------------")
-                logging.info(partner.name)
-                self.create({'user_id': self.user_id.id, 'partner_name': partner.name, 'visit_next_day': visit.next_visit})
-
-        #         logging.info("--------")
-        #         logging.info(visit.week_day)
-        #         logging.info(visit.order)
-        #         logging.info(visit.period)
-        #         logging.info(visit.next_visit)
+    # @api.onchange('user_id')
+    # def _onchange_user_id(self):
+    #     logging.info("----------------------------------")
+    #     logging.info(self.user_id)
+    #     for partner in self.user_id.partner_ids:
+    #         # logging.info(partner.name)
+    #         for visit in partner.visit_ids:
+    #             logging.info("----------------------------------")
+    #             logging.info(partner.name)
+    #             self.create({'user_id': self.user_id.id, 'partner_name':
+    #             partner.name, 'visit_next_day': visit.next_visit})
+    #
+    #              logging.info("--------")
+    #              logging.info(visit.week_day)
+    #              logging.info(visit.order)
+    #              logging.info(visit.period)
+    #              logging.info(visit.next_visit)
